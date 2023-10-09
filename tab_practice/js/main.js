6ae0b0ef5b82489d183acd6643563f932ea034c7
ichinosekiwa <128582009+ichinosekiwa@users.noreply.github.com>
2023-10-09 11:40:39 +0900

    Initial commit

---
+++ tab_practice/js/main.js
@@ -0,0 +1,21 @@
+'use strict';
+
+const tabs = document.querySelectorAll('.tabs li');
+const contents = document.querySelectorAll('.contents li');
+
+//tabが0〜数だけ繰り返す
+for (let i = 0; i < tabs.length; i++) {
+  //tabをクリックしたら
+  tabs[i].addEventListener('click', function () {
+    //全てのactiveを消す
+    for (let j = 0; j < tabs.length; j++) {
+      tabs[j].classList.remove('active');
+    }
+    for (let j = 0; j < contents.length; j++) {
+      contents[j].classList.remove('active');
+    }
+    //クリックしたtabとcontentにactiveつける
+    tabs[i].classList.add('active');
+    contents[i].classList.add('active');
+  });
+}
