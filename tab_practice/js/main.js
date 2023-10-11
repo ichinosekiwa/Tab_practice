d56195ceed1f3a877e781fb4eecd75394a4b0053
ichinosekiwa <128582009+ichinosekiwa@users.noreply.github.com>
2023-10-11 22:02:15 +0900

    js修正

--- tab_practice/js/main.js
+++ tab_practice/js/main.js
@@ -3,30 +3,13 @@
 const tabs = document.querySelectorAll('.tabs li');
 const contents = document.querySelectorAll('.contents li');
 
-tabs.forEach((tab, item) => {
-  tab.addEventListener('click', () => {
-    tabs.forEach((a) => a.classList.remove('active'));
-    contents.forEach((b) => b.classList.remove('active'));
-    
+tabs.forEach((tab, i) => {
+  tab.addEventListener('click' , function() {
+    //全ての1actioveを消す
+    tabs.forEach((tab) => tab.classList.remove('active'));
+    contents.forEach((content) => content.classList.remove('active'));
+    //クリックしたtabとcontentのactiveをつける
     tab.classList.add('active');
-    contents[item].classList.add('active');
+    contents[i].classList.add('active');
   });
 });
-
-// //tabが0〜数だけ繰り返す
-// for (let i = 0; i < tabs.length; i++) {
-//   //tabをクリックしたら
-//   tabs[i].addEventListener('click', function () {
-//     //全てのactiveを消す
-//     for (let j = 0; j < tabs.length; j++) {
-//       tabs[j].classList.remove('active');
-//     }
-//     for (let j = 0; j < contents.length; j++) {
-//       contents[j].classList.remove('active');
-//     }
-//     //クリックしたtabとcontentにactiveつける
-//     tabs[i].classList.add('active');
-//     contents[i].classList.add('active');
-//   });
-// }
-
