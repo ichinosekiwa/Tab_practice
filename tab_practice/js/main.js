8ba60b2f7607d1f4b5ece4d7c6763046e246a963
ichinosekiwa <128582009+ichinosekiwa@users.noreply.github.com>
2023-10-12 23:27:13 +0900

    本日もできないまま力尽きました・・・

--- tab_practice/js/main.js
+++ tab_practice/js/main.js
@@ -2,6 +2,10 @@
 
 const tabs = document.querySelectorAll('.tabs li');
 const contents = document.querySelectorAll('.contents li');
+const firstTabs = document.querySelector('.first-tabs');
+const secondTabs = document.querySelector('.second-tabs');
+const arrow = document.querySelector('.arrow');
+const arrow2 = document.querySelector('.arrow-2');
 
 tabs.forEach((tab, i) => {
   tab.addEventListener('click' , function() {
@@ -11,5 +15,17 @@ tabs.forEach((tab, i) => {
     //クリックしたtabとcontentのactiveをつける
     tab.classList.add('active');
     contents[i].classList.add('active');
+  });
+});
+
+arrow.forEach((arrow, i) => {
+  arrow.addEventListener('click', function() {
+    //1つ目のarrowをクリックしたら2つ目のarrowをactiveに切り替えたい
+    arrow.forEach((arrow) => arrow.classList.remove('active'));
+    firstTabs.classList.remove('active');
+    secondTabs.classList.remove('active');
+
+    arrow2.classList.add('active');
+    arrow2[i].classList.add('active');
   });
 });
